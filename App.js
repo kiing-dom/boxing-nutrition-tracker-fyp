8e1aff01eb72a60da40252f4fca42590e5622b51
Dominion Gbadamosi <worshipgbadamosi@live.com>
2024-01-26 22:46:22 +0000

    configured tailwind-css and react navigation
    added ProfileCreationScreen base code

--- App.js
+++ App.js
@@ -1,20 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { TailwindProvider } from 'tailwindcss-react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import ProfileCreationScreen from './screens/ProfileCreationScreen';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <TailwindProvider>
+        <Stack.Navigator>
+          {/* Screens */}
+          <Stack.Screen name="ProfileCreation" component={ProfileCreationScreen} />
+        </Stack.Navigator>
+      </TailwindProvider>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
---
+++ screens/ProfileCreationScreen.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native';
+import React from 'react';
+
+const ProfileCreationScreen = () => {
+  return (
+    <View>
+      <Text>ProfileCreationScreen</Text>
+    </View>
+  )
+}
+
+export default ProfileCreationScreen
