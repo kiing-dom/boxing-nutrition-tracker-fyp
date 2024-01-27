df1e865fd0086c570e5599cdd3231795981921a9
Dominion Gbadamosi <worshipgbadamosi@live.com>
2024-01-27 01:25:31 +0000

    more UI work done on LoginScreen

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,22 +1,33 @@
 import { Text, StyleSheet, View } from 'react-native';
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import { Button, Input, Image, Icon } from '@rneui/themed';
 import { StatusBar } from 'expo-status-bar';
 
-export default class LoginScreen extends Component {
-  render() {
+const LoginScreen = () => {
+  //TODO: add the rest of the state variables
+  const [email, setEmail] = useState('');
+
     return (
       <View>
         <StatusBar style='light' />
         <Icon
-            name='lock'
-            type='material'
+            name='lock-closed'
+            type='ionicon'
             color='#8868BD'
+            size={150}
         />
+        <View style={styles.inputContainer}>
 
+          <Input placeholder='Email' type="Email" />
+          <Input placeholder='Password' secureTextEntry type="password"/>
+
+        </View>
       </View>
     )
-  }
-};
+  };
 
-const styles = StyleSheet.create({})
+  export default LoginScreen;
+
+const styles = StyleSheet.create({
+  inputContainer: {},
+});
